ef472e309a8210905aa77a6f54289e9f4a11d75e
Tao <taole.chen@gmail.com>
2026-08-29 19:13:25 +0200

    attempt fix tailwind with webpack minimze bypass for next 15.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,14 @@
 const config = {
+  // Add this block to bypass the buggy Next 15 minifier
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: false, 
+      };
+    }
+    return config;
+  },
   plugins: {
     '@tailwindcss/postcss': {}, // Updated for v4
   },
