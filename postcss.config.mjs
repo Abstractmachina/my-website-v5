1cd517a7e34205ec48649690d087d64b538da1d8
Tao <taole.chen@gmail.com>
2026-08-29 23:46:03 +0200

    clean up postss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,14 +1,4 @@
 const config = {
-  // Add this block to bypass the buggy Next 15 minifier
-  webpack: (config, { dev }) => {
-    if (!dev) {
-      config.optimization = {
-        ...config.optimization,
-        minimize: false, 
-      };
-    }
-    return config;
-  },
   plugins: {
     '@tailwindcss/postcss': {}, // Updated for v4
   },
